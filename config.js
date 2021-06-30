a7cddd6a57220d93cdbbd2f9963fd3f6fb3e1d40
Tranhung12 <86721468+Tranhung12@users.noreply.github.com>
2021-06-30 18:53:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: ' bé khủng long cute à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    mà sao em cứ đi nhầm đường
+    lạc vào tim anh lẻ loi
+    đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là thích anh ',
+    btnNo: 'Không, Anh trai à -.-',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh :<',
+    btnReply: 'nói cho anh biết i',
     reply: 'Yêu thì yêu mà không yêu thì yêu',
     mess: 'Anh biết mà. hihii',
-    messDesc: 'anh yêu em .',
+    messDesc: 'ANH YÊU EM .',
     btnAccept: 'em yêu anh',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
