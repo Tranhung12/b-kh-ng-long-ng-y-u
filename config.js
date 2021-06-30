dadb63e3666058ef266241febda7b263dc9d6a11
Tranhung12 <86721468+Tranhung12@users.noreply.github.com>
2021-06-30 19:00:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,12 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh ',
+    btnYes: 'em thích anh ',
     btnNo: 'Không, Anh trai à -.-',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh :<',
     btnReply: 'nói cho anh biết i',
     reply: 'Yêu thì yêu mà không yêu thì yêu',
     mess: 'Anh biết mà. hihii',
-    messDesc: 'ANH YÊU EM .',
     btnAccept: 'em yêu anh',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
