f4bb5ff10198078872ea190c7c55dadca0ae805e
Tranhung12 <86721468+Tranhung12@users.noreply.github.com>
2021-07-17 20:31:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: ' pé ơiii',
+    introTitle: 'cậu ơiii',
     introDesc: `Trái đất vốn lạ thường
-    mà sao em cứ đi nhầm đường
-    lạc vào tim anh lẻ loi
+    mà sao cậu cứ đi nhầm đường
+    lạc vào tim tui lẻ loi
     đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'đừng có hong đấy nha',
-    desc: 'em có biết em dễ thương lắm hong  ',
+    desc: 'cậu có biết cậu dễ thương lắm hong ? ',
     btnYes: 'em biết mà   ',
-    btnNo: 'Không, Anh trai à -.-',
-    question:'em đừng để người cũ làm em tức cái lồn ngực nữa nhaaa! ',
-    btnReply: 'em bít ròi ',
-    reply: 'em mở lòng ra đi nha đâu phải ai cũng xấu âu',
-    messDesc: 'hiểu hemm?',
-    btnAccept: 'hiểu ròi',
+    btnNo: 'Không, cậu ơi -.-',
+    question:'giữa tui và con cá cậu chọn aiii ? ',
+    btnReply: 'tui bít ròi ',
+    reply: 'phải chọn tuiii nghee hongg  ',
+    messDesc: 'chọn tui ii. Mót tui đem con cá và tui về chơi với bà ',
+    btnAccept: 'thật 100%',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
