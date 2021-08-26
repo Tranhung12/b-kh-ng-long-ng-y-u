db198b61fc1b34e2d2add47a3c72b16a400edf31
Tranhung12 <86721468+Tranhung12@users.noreply.github.com>
2021-08-26 15:33:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const CONFIG = {
     btnYes: 'có , được chưaaa -.-   ',
     btnNo: 'Không, em à !',
     question:'biết sao em thương chị honggg ?',
-    btnReply: 'tại chị dễ thương lắm lắm lunnn á',
-    reply: 'có xạo quá hong đó  ',
+    btnReply: 'có xạo quá không vậy',
+    reply: 'tại chị dễ thương lắm lắm lunnn á  ',
     messDesc: 'thật lòng mà nếu có chị em sẽ làm mọi cách để chị yêu thương em hoài luôn hehee',
-    btnAccept: 'thật 100%',
+    btnAccept: 'thật 100%, yêu em đi rồi em là của chị từ nay về sau',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
