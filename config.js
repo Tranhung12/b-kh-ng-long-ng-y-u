c3faf0084c44a7002487571357e58b0f288a8c05
Tranhung12 <86721468+Tranhung12@users.noreply.github.com>
2022-01-18 14:49:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
-    introTitle: 'chị ơiii',
-    introDesc: `chị có biết em thương chị nhiều lắm hong ?`,
+    introTitle: 'Ly ơiii',
+    introDesc: `em có biết anh thương em nhiều lắm hong ?`,
     btnIntro: 'hihi',
-    title: 'chị có thương yêu em honggg ?',
+    title: 'em có thương yêu anh honggg ?',
     desc: 'đừng có hong đó nhaa ',
     btnYes: 'có , được chưaaa -.-   ',
-    btnNo: 'Không, em à !',
-    question:'biết sao em thương chị honggg ?',
+    btnNo: 'Không, anh à !',
+    question:'biết sao anh thương em honggg ?',
     btnReply: 'có xạo quá không vậy',
-    reply: 'tại chị dễ thương lắm lắm lunnn á  ',
-    messDesc: 'thật lòng mà nếu có chị em sẽ làm mọi cách để chị yêu thương em hoài luôn hehee',
-    btnAccept: 'thật 100%, yêu em đi rồi em là của chị từ nay về sau',
+    reply: 'tại Ly dễ thương lắm lắm lunnn á  ',
+    messDesc: 'thật lòng mà nếu có em thì anh sẽ làm mọi cách để em yêu anh hoài luôn hehee',
+    btnAccept: 'thật 100%, yêu anh đi rồi anh là của em từ nay về sau',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
